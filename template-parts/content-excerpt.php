d2f1cd7aa602624eedaa2614da42c0fc824557c1
Greg <grze.ndz.14@gmail.com>
2015-05-27 04:56:01 +0200

    Post index changes.

--- template-parts/content-excerpt.php
+++ template-parts/content-excerpt.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 
@@ -19,14 +19,7 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-		/* translators: %s: Name of current post */
-//		the_content( sprintf(
-//			wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'gently' ), array( 'span' => array( 'class' => array() ) ) ),
-//			the_title( '<span class="screen-reader-text">"', '"</span>', false )
-//		) );
-		the_excerpt();
-		?>
+		<?php the_excerpt(); ?>
 
 	</div><!-- .entry-content -->
 
