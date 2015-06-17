2a911d8a550f4c3b373b2dd74acc94081486aa36
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2015-06-17 20:16:39 +0200

    Added TGMPA back end message.

--- inc/tgm-plugin-activation.php
+++ inc/tgm-plugin-activation.php
@@ -8,19 +8,19 @@ function my_theme_register_required_plugins() {
     $plugins = array(
         //
         array(
-            'name'      => 'Kirki customizer',
-            'slug'      => 'kirki',
-            'required'  => true,
+            'name'     => 'Kirki customizer',
+            'slug'     => 'kirki',
+            'required' => true,
         ),
 		array(
-			'name'      => 'Mailchimp for WordPress',
-			'slug'      => 'mailchimp-for-wp',
-			'required'  => false
+			'name'     => 'Mailchimp for WordPress',
+			'slug'     => 'mailchimp-for-wp',
+			'required' => false
 		)
     );
 
     $config = array(
-        'message'      => '<h1>All recommended plugins.</h1>'
+        'message'      => '<div class="update-nag"><h4 style="color:#ce1714">Kirki is mandatory for theme functionality.</h4><p><strong>All other recommended plugins are tested to use with theme, install that as you need.</strong></p></div>'
     );
 
     tgmpa( $plugins, $config );
