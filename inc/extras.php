9251c97a06c376a97fdc7eec5a78ddb129e40397
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2015-11-07 20:50:02 +0100

    Remove obsolete functions.

--- inc/extras.php
+++ inc/extras.php
@@ -55,55 +55,6 @@ function genlty_sidebar_position( $classes ) {
 
 add_filter( 'body_class', 'genlty_sidebar_position' );
 
-if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
-	/**
-	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
-	 *
-	 * @param string $title Default title text for current view.
-	 * @param string $sep Optional separator.
-	 *
-	 * @return string The filtered title.
-	 */
-	function gently_wp_title( $title, $sep ) {
-		if ( is_feed() ) {
-			return $title;
-		}
-
-		global $page, $paged;
-
-		// Add the blog name
-		$title .= get_bloginfo( 'name', 'display' );
-
-		// Add the blog description for the home/front page.
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) ) {
-			$title .= " $sep $site_description";
-		}
-
-		// Add a page number if necessary:
-		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-			$title .= " $sep " . sprintf( esc_html__( 'Page %s', 'gently' ), max( $paged, $page ) );
-		}
-
-		return $title;
-	}
-
-	add_filter( 'wp_title', 'gently_wp_title', 10, 2 );
-
-	/**
-	 * Title shim for sites older than WordPress 4.1.
-	 *
-	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 */
-	function gently_render_title() {
-		?>
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<?php
-	}
-
-	add_action( 'wp_head', 'gently_render_title' );
-endif;
 
 if ( ! function_exists( 'gently_excerpt_more' ) ) :
 	/**
